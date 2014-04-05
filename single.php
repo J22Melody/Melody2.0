c3e3fd30d57dffa75a9a25d5cbbe7bcef9cb363c
JiangZifan <jiangzifan@JZFdeMacBook-Pro.local>
2014-04-05 11:22:25 +0800

    add 2 pic and add gitignore and change time position

--- single.php
+++ single.php
@@ -3,16 +3,19 @@
 <?php the_post() ?>
 
 <div id="title">
-    <p class="create-time"><?php the_date(); ?></p>
     <h1><?php the_title(); ?></h1>
     <nav>
         <a href="<?php bloginfo('url'); ?>">首页</a>
-        <a href="<?php bloginfo('url'); ?>/?page_id=2">关于我</a>
-        <a href="<?php bloginfo('url'); ?>/?page_id=20">所有文章</a>
+        <a href="<?php bloginfo('url'); ?>/?page_id=225">关于我</a>
+        <a href="<?php bloginfo('url'); ?>/?page_id=372">所有文章</a>
     </nav>
 </div>
 
 <div class="post-content"><?php the_content(); ?></div>  
+
+<div class="clearfix">
+    <p class="create-time pull-right"><?php the_date(); ?></p>
+</div>
 
 <div class="post-navigation">
     <div class="pull-left"><?php previous_post_link( '%link', '&laquo; 上一篇：%title' ) ?></div>
