e69b97752b9a94b040d3f0fba8f6ac54e768599c
蒋子凡 <jzf@zhihu.com>
2015-03-10 18:20:30 +0800

    about placeholder

--- about.php
+++ about.php
@@ -8,13 +8,15 @@ Template Name: About
 
 <div class="container">
 
-    <div class="archives">
+    <div class="inner">
+        <a href="<?php echo get_site_url(); ?>" class="home-link">Home</a>
+
         <div class="top">
-            <h1><?php the_title(); ?></h1>
+            <h1>关于</h1>
         </div>
 
         <div class="content">
-            about me
+            正在写 Orz ...
         </div>
     </div>
 </div>
