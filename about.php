81fba539cf431e3e6f4f3964191018fb0fd308a7
蒋子凡 <jzf@zhihu.com>
2015-04-04 14:25:39 +0800

    add projects to about

--- about.php
+++ about.php
@@ -15,31 +15,32 @@ Template Name: About
             <h1>关于</h1>
         </div>
 
-        <div class="content">
-            <p>
+        <div class="content about-content">
+            <section>
                 理想主义者，
-            </p>
-            <p>
-                悲观主义者，
-            </p>
-            <p>
-                享乐主义者。
-            </p>
-            <br>
-            <p>
+                <br>悲观主义者，
+                <br>享乐主义者。
+            </section>
+
+            <section>
                 希腊神话说，人啊，认识你自己。关于我是一个什么样的人，这亦是我一生所要探寻的东西。
-            </p>
-            <p>
-                我最近的一份工作是在知乎，担任前端开发。而现在我已经回到学校，处理一些闲杂的事情，以及筹划一场毕业旅行。
-            </p>
-            <p>
-                哦，如果顺利的话，我将于 2015 夏天毕业于南京大学。
-            </p>
-            <br>
-            <p>
+                <br>我最近的一份工作是在知乎，担任前端开发。而现在我已经回到学校，处理一些闲杂的事情，以及筹划一场毕业旅行。
+                <br>哦，如果顺利的话，我将于 2015 夏天毕业于南京大学。
+            </section>
+
+            <section>
                 订阅蒋子凡的博客：
-            </p>
-            <?php echo do_shortcode("[jetpack_subscription_form]"); ?>
+                <br><?php echo do_shortcode("[jetpack_subscription_form]"); ?>
+            </section>
+
+            <section>
+                <h3>PROJECTS</h3>
+                <ul>
+                    <li>仙林软件奇侠传，背景为南大的 RPG 游戏，Based on Java Swing</li>
+                    <li><a href="http://douya.jzf.life" target="_blank">豆芽</a>，仿豆瓣足迹的旅行网站，Powered by Django</li>
+                    <li><a href="http://classic.lilystudio.org" target="_blank">ClassIC</a>，课程分享网站，<a href="http://jzf.life/archive/500" target="_blank">《ClassIC I See》</a>，Powered by Yii</li>
+                </ul>
+            </section>
         </div>
     </div>
 </div>
